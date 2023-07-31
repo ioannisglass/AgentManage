b1345f3817cfa5f20ce3d9e82a2a09dd0c3a59f4
SU Glass <suglass1128@gmail.com>
2023-07-31 09:18:54 +0300

    New page to uninstall applications

---
+++ app-react-user/src/components/pages/RemAppPage.js
@@ -0,0 +1,176 @@
+import React, { useEffect, useState } from 'react';
+
+import API from "../../api/api";
+import { useParams, useNavigate } from 'react-router';
+
+const RemAppPage = () => {
+    
+    const [tableData, setTableData] = useState([]);
+    const [checkboxes, setCheckboxes] = useState([]);
+    const { aid, app } = useParams();
+    const navigate = useNavigate();
+    const [showModal, setShowModal] = useState(false);
+
+    const getSelectedValues = () => {
+        const selectedValues = checkboxes
+          .filter((checkbox) => checkbox.checked)
+          .map((checkbox) => checkbox.host);
+        return selectedValues;
+    };
+
+    const handleCheckboxChange = (event, id) => {
+        const updatedCheckboxes = checkboxes.map((checkbox) =>
+          checkbox.id === id
+            ? { ...checkbox, checked: event.target.checked }
+            : checkbox
+        );
+        setCheckboxes(updatedCheckboxes);
+    };
+
+    const confirmDelete = () => {
+        setShowModal(false);
+        const selectedValues = getSelectedValues();
+        console.log('Selected Values:', selectedValues);
+
+        API.uninstallApps({
+            aid: aid,
+            app: app,
+            hosts: selectedValues
+        })
+        .then(
+            navigate(`/allapp/${aid}`)
+        )
+    }
+    const handleDelete = () => {
+        setShowModal(true);
+    }
+
+    useEffect(() => {
+        API.getAgentsToRem(aid, app)
+            .then((res) => { 
+                const initialCheckboxes = []
+                res.data.map((one, _i) => {
+                    initialCheckboxes.push({
+                        ...one,
+                        checked: false
+                    })
+                });
+                setCheckboxes(initialCheckboxes);
+             })
+             .catch((err) => {
+                console.log(err);
+             })
+    }, [])
+
+    return (
+        <>
+            <div className='m-32'>
+                <div className='grid grid-cols-2 gap-4 p-2'>
+                    <div className='text-white text-2xl'>Detail</div>
+                    <div className='text-white justify-self-end'>
+                        <button 
+                            type="button" 
+                            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                            onClick={() => handleDelete()}
+                        >
+                            Uninstall
+                        </button>
+                        <button
+                            type="button" 
+                            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                            onClick={() => { navigate(-1) }}
+                        >
+                            Back
+                        </button>
+                    </div>
+                </div>
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    Device
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    <span className="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                checkboxes.map((one, _i) => 
+                                    <tr className="bg-white border-t dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={_i}>
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            { one.host }
+                                        </th>
+                                        <td className="px-6 py-4 text-right">
+                                            <label>
+                                                <input
+                                                    type="checkbox"
+                                                    checked={one.checked}
+                                                    onChange={(event) => handleCheckboxChange(event, one.id)}
+                                                />
+                                            </label>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            {showModal ? (
+                <>
+                    <div className="fixed inset-0 z-10 overflow-y-auto">
+                        <div
+                            className="fixed inset-0 w-full h-full bg-black opacity-40"
+                            onClick={() => setShowModal(false)}
+                        ></div>
+                        <div className="flex items-center min-h-screen px-4 py-8">
+                            <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
+                                <div className="mt-3 sm:grid">
+                                    <div className="flex items-center justify-center flex-none w-12 h-12 mx-auto bg-red-100 rounded-full">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="w-6 h-6 text-red-600"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                    </div>
+                                    <div className="mt-2 text-center sm:ml-4 sm:text-left">
+                                        <div className="items-center gap-2 mt-3 sm:flex">
+                                            <button
+                                                className="w-full mt-2 p-2.5 flex-1 text-white bg-red-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
+                                                onClick={() =>
+                                                    confirmDelete()
+                                                }
+                                            >
+                                                Uninstall
+                                            </button>
+                                            <button
+                                                className="w-full mt-2 p-2.5 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
+                                                onClick={() =>
+                                                    setShowModal(false)
+                                                }
+                                            >
+                                                Cancel
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ) : null}
+        </>
+    )
+}
+
+export default RemAppPage;
