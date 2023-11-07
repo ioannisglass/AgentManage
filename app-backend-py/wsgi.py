3f208fe1b65546294623cd297e9e2dd200c7c2b8
SU Glass <suglass1128@gmail.com>
2023-11-07 16:25:19 +0200

    wsgi, package added

---
+++ app-backend-py/wsgi.py
@@ -0,0 +1,11 @@
+# import imp
+# import os
+# import sys
+
+
+# sys.path.insert(0, os.path.dirname(__file__))
+
+# wsgi = imp.load_source('wsgi', 'main.py')
+# application = wsgi.application
+
+from main import app as application
